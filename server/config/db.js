732c0a4cec94d987855dda92d3c202b68f041df6
Shaktimayee-Mohanty <jasmine21102004@gmail.com>
2026-01-26 11:43:18 +0530

    3rd commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
-const connectDB=async ()=>{
-    try{
-        mongoose.connection.on('connected',()=>console.log("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/hotel-app`)
-    } catch(error){
-        console.log(error.message);
-    }
-}
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database Connected")
+    );
 
+    await mongoose.connect(process.env.MONGODB_URI);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export default connectDB;
